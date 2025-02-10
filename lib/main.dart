69bc67b1f8e84f2d38181608fc1cf5855259bbed
shaha <shohruh300908@gmail.com>
2025-02-10 22:07:39 +0500

    v2 auth

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChatPage(),
+      home: AuthPage(),
     );
   }
 }
